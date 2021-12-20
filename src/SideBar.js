31f3553e6cbe2f1501b07729bab25ebc5c2bd588
Patrick Yambrick <patrickyambrick@gmail.com>
2021-12-20 18:50:33 -0500

    Fix sidebar/grid overlap

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,6 +1,6 @@
 function SideBar() {
   return(
-    <div className="sidebar md:col-span-3 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+    <div className="sidebar md:col-span-3 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
       <nav>
       <a href="#Home" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
         Home
